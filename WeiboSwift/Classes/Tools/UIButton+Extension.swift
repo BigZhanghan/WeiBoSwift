ab117c26a713be37df66a4e85f4aa4a794fa4c83
zhanghan <mmzhanghanhan@163.com>
2022-09-28 13:57:00 +0800

    TabBarController布局

--- WeiboSwift/Classes/Tools/UIButton+Extension.swift
+++ WeiboSwift/Classes/Tools/UIButton+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by zhanghan on 2022/9/28.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - UIButton扩展
+extension UIButton {
+
+    convenience init(_ imageName: String, _ backImageName: String) {
+        self.init()
+        setImage(UIImage(named: imageName), for: .normal)
+        setImage(UIImage(named: imageName + "_highlighted"), for: .highlighted)
+        setBackgroundImage(UIImage(named: backImageName), for: .normal)
+        setBackgroundImage(UIImage(named: backImageName + "_highlighted"), for: .highlighted)
+        sizeToFit()
+    }
+}
--- WeiboSwift/Classes/View/Main/MainTabBarController.swift
+++ WeiboSwift/Classes/View/Main/MainTabBarController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+    
+    private lazy var composeBtn: UIButton = {
+        let btn = UIButton("tabbar_compose_icon_add", "tabbar_compose_button")
+        btn.addTarget(self, action: #selector(clickComposeBtn), for: .touchUpInside)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addChildViewControllers()
+        setupComposeBtn()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tabBar.bringSubviewToFront(composeBtn)
     }
-    */
+    
+    /// 点击事件
+    @objc private func clickComposeBtn() {
+        print("点我了")
+    }
+}
 
+extension MainTabBarController {
+    
+    private func setupComposeBtn() {
+        tabBar.addSubview(composeBtn)
+        
+        let width = tabBar.bounds.width / CGFloat(children.count) - 1
+        composeBtn.frame = CGRectInset(tabBar.bounds, width * 2, 0)
+        
+    }
+
+    private func addChildViewControllers() {
+        addChildViewController(HomeTableViewController(), "首页", "tabbar_home")
+        addChildViewController(MessageTableViewController(), "消息", "tabbar_message")
+        addChild(UIViewController())
+        addChildViewController(DiscoverTableViewController(), "发现", "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), "我的", "tabbar_profile")
+        
+        tabBar.tintColor = .orange
+    }
+    
+    private func addChildViewController(_ vc: UIViewController, _ title: String, _ imageName: String) {
+        vc.tabBarItem.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        let nav = UINavigationController(rootViewController: vc)
+        addChild(nav)
+    }
 }
